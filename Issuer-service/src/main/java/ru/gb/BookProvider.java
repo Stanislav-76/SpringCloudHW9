c8f15a84f62c67493db0be8010ba2c494b12a4fd
Stanislav <redkorona@yandex.ruggggggggggggggg>
2024-02-11 03:04:40 +0300

    HW9

---
+++ Issuer-service/src/main/java/ru/gb/BookProvider.java
@@ -0,0 +1,41 @@
+package ru.gb;
+
+import org.springframework.cloud.client.loadbalancer.reactive.ReactorLoadBalancerExchangeFilterFunction;
+import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
+import ru.gb.api.Book;
+
+import java.util.UUID;
+
+@Service
+public class BookProvider {
+
+    private final WebClient webClient;
+
+
+    public BookProvider(ReactorLoadBalancerExchangeFilterFunction loadBalancerExchangeFilterFunction) {
+        webClient = WebClient.builder()
+                .filter(loadBalancerExchangeFilterFunction)
+                .build();
+    }
+
+    public UUID getRandomBookId() {
+        Book randomBook = webClient.get()
+                .uri("http://book-service/api/book/random")
+                .retrieve()
+                .bodyToMono(Book.class)
+                .block();
+
+        return randomBook.getId();
+    }
+    public Book getRandomBook() {
+        Book randomBook = webClient.get()
+                .uri("http://book-service/api/book/random")
+                .retrieve()
+                .bodyToMono(Book.class)
+                .block();
+
+        return randomBook;
+    }
+
+}
